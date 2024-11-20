25998f6f8d616080ed0cb30eddb2ea49df385a13
E. Hannah <echannah631@gmail.com>
2024-11-20 15:30:25 -0500

    displaying a remote image on dynamic page, updated nextjs config to accept images from hostname

--- app/tours/[id]/page.tsx
+++ app/tours/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react';
 
 type SpecificTourPageProps = {
@@ -6,10 +7,25 @@ type SpecificTourPageProps = {
   };
 };
 
+const url = 'https://www.course-api.com/images/tours/tour-1.jpeg';
+
 const SpecificTourPage = ({ params }: SpecificTourPageProps) => {
   return (
     <div>
       <h1 className='text-4xl'>ID: {params.id}</h1>
+      <section className='flex gap-x-4 mt-4'>
+        <div>
+          <Image
+            className='w-48 h-48 object-cover rounded'
+            src={url}
+            alt='tour'
+            width={192}
+            height={192}
+            priority
+          />
+          <p>remote image</p>
+        </div>
+      </section>
     </div>
   );
 };
